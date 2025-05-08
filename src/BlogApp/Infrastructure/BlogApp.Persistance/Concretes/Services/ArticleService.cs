06e758c7e801d9d37072118b6c8fac98de47e034
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-08 14:34:46 +0300

    feat(Infr): basic implement article service and repository

---
+++ src/BlogApp/Infrastructure/BlogApp.Persistance/Concretes/Services/ArticleService.cs
@@ -0,0 +1,42 @@
+﻿using BlogApp.Application.Abstractions.Repositories;
+using BlogApp.Application.Abstractions.Services;
+using BlogApp.Domain.Entities;
+
+namespace BlogApp.Persistance.Concretes.Services
+{
+    public class ArticleService : IArticleService
+    {
+        private readonly IArticleRepository _articleRepository;
+
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }
+
+
+        public async Task<bool> CreateArticle(Article article)
+        {
+            return _articleRepository.CreateArticle(article);
+        }
+
+        public async Task DeleteArticle(string id)
+        {
+            _articleRepository.DeleteArticle(id);
+        }
+
+        public async Task<Article> GetArticleByID(string id)
+        {
+            return _articleRepository.GetArticleById(id);
+        }
+
+        public async Task UpdateArticle(string id, Article article)
+        {
+            _articleRepository.UpdateArticle(id, article);
+        }
+
+        public List<Article> GetAll()
+        {
+            return _articleRepository.GetAll();
+        }
+    }
+}
---
+++ src/BlogApp/Infrastructure/BlogApp.Persistance/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Application.Abstractions.Repositories;
+using BlogApp.Application.Abstractions.Services;
+using BlogApp.Persistance.Concretes.Repositories;
+using BlogApp.Persistance.Concretes.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlogApp.Persistance
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistanceService(this IServiceCollection services)
+        {
+            services.AddSingleton<IArticleService, ArticleService>();
+            services.AddSingleton<IArticleRepository, ArticleRepository>();
+        }
+    }
+}
