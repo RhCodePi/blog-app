b3bb03cbb5b91869b5f1774c976f07f365901bea
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-08 14:29:39 +0300

    feat(application): add basic article interfaces

---
+++ src/BlogApp/Core/BlogApp.Application/Abstractions/Repositories/IArticleRepository.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.Domain.Entities;
+
+namespace BlogApp.Application.Abstractions.Repositories
+{
+    public interface IArticleRepository
+    {
+        public bool CreateArticle(Article article);
+        public void DeleteArticle(string id);
+        public void UpdateArticle(string id, Article updatedArticle);
+        public Article GetArticleById(string id);
+        public List<Article> GetAll();
+    }
+}
---
+++ src/BlogApp/Core/BlogApp.Application/Abstractions/Services/IArticleService.cs
@@ -0,0 +1,13 @@
+﻿using BlogApp.Domain.Entities;
+
+namespace BlogApp.Application.Abstractions.Services
+{
+    public interface IArticleService
+    {
+        public Task<bool> CreateArticle(Article article);
+        public Task DeleteArticle(string id);
+        public Task UpdateArticle(string id, Article article);
+        public Task<Article> GetArticleByID(string id);
+        public List<Article> GetAll();
+    }
+}
