1f6ad6444a1b48a739b0ecea82532afc063dcc4d
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-15 17:26:34 +0300

    feat: add login endpoint

---
+++ src/BlogApp/Presentation/BlogApp.API/Controller/AuthsController.cs
@@ -0,0 +1,26 @@
+﻿using BlogApp.Application.Abstractions.Services;
+using BlogApp.Application.DTOs.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthsController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthsController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto model)
+        {
+            var result = await _authService.Login(model);
+
+            return Ok(result);
+        }
+    }
+}
