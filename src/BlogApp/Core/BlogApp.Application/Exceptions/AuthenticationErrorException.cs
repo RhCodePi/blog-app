bbf3d905d4e907e521e836df1a95ad2fe7ef0a8d
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-17 22:15:18 +0300

    feat: add auth error exception

---
+++ src/BlogApp/Core/BlogApp.Application/Exceptions/AuthenticationErrorException.cs
@@ -0,0 +1,17 @@
+﻿namespace BlogApp.Application.Exceptions
+{
+    public class AuthenticationErrorException : Exception
+    {
+        public AuthenticationErrorException() : base("authentication error")
+        {
+        }
+
+        public AuthenticationErrorException(string? message) : base(message)
+        {
+        }
+
+        public AuthenticationErrorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
