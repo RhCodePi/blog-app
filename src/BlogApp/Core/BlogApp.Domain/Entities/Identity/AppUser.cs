81c8b4bcb3a32cebf11b72eb7e155e91cc2eb165
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-17 22:01:02 +0300

    feat(user): update entity and database

--- src/BlogApp/Core/BlogApp.Domain/Entities/Identity/AppUser.cs
+++ src/BlogApp/Core/BlogApp.Domain/Entities/Identity/AppUser.cs
@@ -4,6 +4,7 @@ namespace BlogApp.Domain.Entities.Identity
 {
     public class AppUser: IdentityUser<string> 
     {
-
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenEndDate { get; set; }
     }
 }
--- src/BlogApp/Infrastructure/BlogApp.Persistance/Migrations/20250516185317_mig_v3.Designer.cs
+++ src/BlogApp/Infrastructure/BlogApp.Persistance/Migrations/20250516185317_mig_v3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlogApp.Persistance.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlogApp.Persistance.Migrations
 {
     [DbContext(typeof(BlogAppDBContext))]
-    partial class BlogAppDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250516185317_mig_v3")]
+    partial class mig_v3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
