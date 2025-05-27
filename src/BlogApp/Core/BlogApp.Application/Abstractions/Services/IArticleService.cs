7e1eda31e4d77e16f1be122500384b940d2a7311
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-05-27 18:29:00 +0300

    feat: add GetUserArticles and refactor CreateArticle

--- src/BlogApp/Core/BlogApp.Application/Abstractions/Services/IArticleService.cs
+++ src/BlogApp/Core/BlogApp.Application/Abstractions/Services/IArticleService.cs
@@ -1,13 +1,16 @@
-﻿using BlogApp.Domain.Entities;
+﻿using BlogApp.Application.DTOs.Article;
+using BlogApp.Application.DTOs.Article.Response;
+using BlogApp.Domain.Entities;
 
 namespace BlogApp.Application.Abstractions.Services
 {
     public interface IArticleService
     {
-        public Task<bool> CreateArticleAsync(Article article);
+        public Task<bool> CreateArticleAsync(CreateArticleDTO model);
         public Task<bool> RemoveArticleAsync(string id);
         public Task<bool> UpdateArticleAsync(string id, Article article);
         public Task<Article> GetArticleByID(string id);
         public List<Article> GetAll();
+        public Task<List<GetUserArticlesResponse>> GetUserArticles(GetUserArticlesDTO model);
     }
 }
--- src/BlogApp/Infrastructure/BlogApp.Persistance/Concretes/Services/ArticleService.cs
+++ src/BlogApp/Infrastructure/BlogApp.Persistance/Concretes/Services/ArticleService.cs
@@ -1,7 +1,13 @@
 ﻿using BlogApp.Application.Abstractions.Repositories;
 using BlogApp.Application.Abstractions.Repositories.Articles;
 using BlogApp.Application.Abstractions.Services;
+using BlogApp.Application.DTOs.Article;
+using BlogApp.Application.DTOs.Article.Response;
+using BlogApp.Application.Exceptions;
 using BlogApp.Domain.Entities;
+using BlogApp.Domain.Entities.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace BlogApp.Persistance.Concretes.Services
 {
@@ -9,15 +15,31 @@ namespace BlogApp.Persistance.Concretes.Services
     {
         private readonly IArticleReadRepository _articleReadRepository;
         private readonly IArticleWriteRepository _articleWriteRepository;
+        private readonly IUserService _userService;
 
-        public ArticleService(IArticleReadRepository articleReadRepository, IArticleWriteRepository articleWriteRepository)
+        public ArticleService(IArticleReadRepository articleReadRepository, IArticleWriteRepository articleWriteRepository, IUserService userService)
         {
             _articleReadRepository = articleReadRepository;
             _articleWriteRepository = articleWriteRepository;
+            _userService = userService;
         }
 
-        public async Task<bool> CreateArticleAsync(Article article)
+        public async Task<bool> CreateArticleAsync(CreateArticleDTO model)
         {
+            AppUser? user = await _userService.GetUserWithRefreshToken(model.RefreshToken);
+
+            if (user == null) throw new UserNotFoundException("User not found");
+
+
+            Article article = new()
+            {
+                Id = Guid.NewGuid(),
+                Content = model.Content,
+                Title = model.Title,
+                UserID = user.Id,
+                CreateDate = DateTime.UtcNow
+            };
+
             var result = await _articleWriteRepository.AddAsync(article);
 
             await _articleWriteRepository.SaveAsync();
@@ -59,7 +81,25 @@ namespace BlogApp.Persistance.Concretes.Services
 
         public List<Article> GetAll()
         {
-            return _articleReadRepository.GetAll().ToList();
+            return [.. _articleReadRepository.GetAll()];
+        }
+
+        public async Task<List<GetUserArticlesResponse>> GetUserArticles(GetUserArticlesDTO model)
+        {
+            var user = await _userService.GetUserWithRefreshToken(model.RefreshToken);
+
+            if (user == null) throw new UserNotFoundException();
+
+            var result = _articleReadRepository.GetWhere(x => x.UserID == user.Id).Select(x =>  new GetUserArticlesResponse
+            {
+                Title = x.Title,
+                Content = x.Content,
+
+            }).ToList();
+
+            if (result.Count == 0) return [];
+
+            return result;
         }
     }
 }
