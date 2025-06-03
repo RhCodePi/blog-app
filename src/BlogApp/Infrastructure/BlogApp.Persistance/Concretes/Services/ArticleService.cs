e7b8420b49b56b761652c1fed380e404837a185c
Abdullah <79790661+RhCodePi@users.noreply.github.com>
2025-06-03 22:03:18 +0300

    feat: use the data time formatter

--- src/BlogApp/Infrastructure/BlogApp.Persistance/Concretes/Services/ArticleService.cs
+++ src/BlogApp/Infrastructure/BlogApp.Persistance/Concretes/Services/ArticleService.cs
@@ -1,27 +1,29 @@
-﻿using BlogApp.Application.Abstractions.Repositories;
-using BlogApp.Application.Abstractions.Repositories.Articles;
+﻿using BlogApp.Application.Abstractions.Repositories.Articles;
 using BlogApp.Application.Abstractions.Services;
 using BlogApp.Application.DTOs.Article;
 using BlogApp.Application.DTOs.Article.Response;
 using BlogApp.Application.Exceptions;
 using BlogApp.Domain.Entities;
 using BlogApp.Domain.Entities.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Text.Json;
 
 namespace BlogApp.Persistance.Concretes.Services
 {
     public class ArticleService : IArticleService
     {
+        private readonly string UNCHANGED = "Unchanged";
+
         private readonly IArticleReadRepository _articleReadRepository;
         private readonly IArticleWriteRepository _articleWriteRepository;
         private readonly IUserService _userService;
+        private readonly IDateTimeFormatter _dateFormatter;
 
-        public ArticleService(IArticleReadRepository articleReadRepository, IArticleWriteRepository articleWriteRepository, IUserService userService)
+
+        public ArticleService(IArticleReadRepository articleReadRepository, IArticleWriteRepository articleWriteRepository, IUserService userService, IDateTimeFormatter dateFormatter)
         {
             _articleReadRepository = articleReadRepository;
             _articleWriteRepository = articleWriteRepository;
             _userService = userService;
+            _dateFormatter = dateFormatter;
         }
 
         public async Task<bool> CreateArticleAsync(CreateArticleDTO model)
@@ -84,17 +86,19 @@ namespace BlogApp.Persistance.Concretes.Services
             return [.. _articleReadRepository.GetAll()];
         }
 
+
         public async Task<List<GetUserArticlesResponse>> GetUserArticles(GetUserArticlesDTO model)
         {
             var user = await _userService.GetUserWithRefreshToken(model.RefreshToken);
 
             if (user == null) throw new UserNotFoundException();
 
-            var result = _articleReadRepository.GetWhere(x => x.UserID == user.Id).Select(x =>  new GetUserArticlesResponse
+            var result = _articleReadRepository.GetWhere(x => x.UserID == user.Id).Select(x => new GetUserArticlesResponse
             {
+                Id = x.Id.ToString(),
                 Title = x.Title,
-                Content = x.Content,
-
+                CreatedDate = _dateFormatter.ConvertToString(x.CreateDate),
+                UpdatedDate = (x.UpdateDate != null ? _dateFormatter.ConvertToString(x.UpdateDate.Value) : UNCHANGED),
             }).ToList();
 
             if (result.Count == 0) return [];
